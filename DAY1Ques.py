526d4cec8e5d5d65038639410570e7dffe483a5f
nishchaya <nishchayavishwakarma@gmail.com>
2026-01-13 10:20:46 +0530

    DAY1&2

---
+++ DAY1Ques.py
@@ -0,0 +1,7 @@
+for i in range(1,21):
+    print(i)
+
+num=range(1,21)
+print(num)
+for i in range(1,21):
+    print(i)
---
+++ DAY2.py
@@ -0,0 +1,20 @@
+'''wap to check if you're eligible to vote'''
+
+x=int(input("enter the number"))
+if x>=18:
+    print("you're eligible to vote")
+else:
+    print("you're not eligible to vote")
+
+
+
+'''Arithmetic Operators'''
+
+x=int(input("enter the number"))
+y=int(input("enter the number"))
+print("addition",x+y)
+print("subtraction",x-y)
+print("multiplication",x*y)
+print("division",x/y)
+print("exponent",x**y)
+print("remainder",x%y)
