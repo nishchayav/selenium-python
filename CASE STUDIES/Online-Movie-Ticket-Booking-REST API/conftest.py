fab4e5bb632f0a28998ee9843b56f174f5e6ed5b
nishchaya <nishchayavishwakarma@gmail.com>
2026-02-13 15:58:42 +0530

    Online-Movie-Ticket-Booking-REST API-CASE STUDY

--- CASE STUDIES/Online-Movie-Ticket-Booking-REST API/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from app import app
-
-@pytest.fixture
-def client():
-    app.testing = True
-    with app.test_client() as client:
-        yield client
--- CASE STUDIES/Online-Movie-Ticket-Booking-REST API/tests/test_bookingAPI.py
+++
@@ -1,6 +0,0 @@
-def test_booking_failure(client):
-    response = client.post('/api/bookings', json={
-        "movie_id": 999,
-        "seats": 2
-    })
-    assert response.status_code == 404
